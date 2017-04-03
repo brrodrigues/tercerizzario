baec2a23a0c977e9afe02387a7d19919d47087f6
Application User <appuser@tercerizzario.localdomain>
2017-04-03 15:51:08 +0000

    Add files to execution

--- tercerizzario-service/run-supplier.sh
+++ tercerizzario-service/run-supplier.sh
@@ -2,51 +2,64 @@
 
 NUM_ARGS=$#
 
-LOG_RUN=~/app/logs/$1.log
-LOG_COMPILE_MAVEN=~/app/logs/"${1}_mvn_compile.log"
+PROJECT_NAME="supplier"
+EUREKA_SERVER=/home/appuser/app/tercerizzario/tercerizzario-service
+APPLICATION_NAME="tercerizzario-service-${PROJECT_NAME}"
+LOG_APPLICATION="$APPLICATION_NAME.log"
+LOG_COMPILE_MAVEN="${APPLICATION_NAME}_mvn_compile.log"
 
+echo 'FINDING LOCATION PATH'
 
-if [ "${NUM_ARGS}" = "0" ]; then
-	echo 'The first argument expected is log filename'
-	exit -1
+if [ ! -d ~/app/logs ]; then
+   echo 'CREATING LOG PATH...'
+   mkdir -p ~/app/logs
 fi
 
-#echo 'FINDING LOG FILE'
+echo "COMPILING PROJECT WITH MAVEN"
 
-#if [ ! -f ~/app/logs ]; then
-#	mkdir -p ~/app/logs
-#fi
+EUREKA_SERVER_PATH=$(pwd)
 
-#echo 'CREATING FILE LOG...'
+if [ -f $LOG_APPLICATION  ]; then
+ rm $LOG_APPLICATION
+fi
 
-#touch ${LOG}
-
-#echo "${LOG}"
-
-echo "COMPILING PROJECT W/ MAVEN"
-
-rm $LOG_COMPILE_MAVEN >> /dev/null
-
-cd tercerizzario-service-server
+if [ -f $LOG_COMPILE_MAVEN ]; then
+  rm $LOG_COMPILE_MAVEN
+fi
 
 mvn clean package >> $LOG_COMPILE_MAVEN
 
-COMPILED=$(grep 'BUILD SUCCESS' $LOG_COMPILE_MAVEN | wc -l)
+COMPILED=$(grep -i 'BUILD FAILED' $LOG_COMPILE_MAVEN | wc -l)
 
-if [ "${COMPILED}" != "1"  ]; then
+if [ "${COMPILED}x" != "0x"  ]; then
 	echo "OCURR ERROR DURING COMPILATION.."
 	echo "SEE MORE IN ${LOG_COMPILE_MAVEN}"
 	exit -1
 fi
 
-echo 'STARTING EUREKA SERVER...'
+echo 'LOCATING APPLICATION PROCESS ID...'
 
-rm $LOG_RUN
+PID=$(ps -ef | grep "${APPLICATION_NAME}" | grep -v grep | awk '{print $2}')
 
-mvn spring-boot:run >> $LOG_RUN
+if [ "${PID}x" = "x" ]; then
+  echo 'DO NOT EXIST APPLICATION SERVICE' 
+else
+  echo "KILLING THE PROCESS ${PID} REFERENCE APPLICATION"
+  kill -9 ${PID}
+  echo "DONE"
+fi
 
-echo 'DONE'
+TARGET=$(ls -a ${EUREKA_SERVER_PATH}/$APPLICATION_NAME/target/${APPLICATION_NAME}*.jar)
+
+if [ ! -f ${TARGET} ]; then
+  echo 'SERVER APPLICATION NOT FOUND'
+  exit -1
+fi
+
+echo 'STARTING ...'
+
+nohup java -Xmx256m -Xss256k -jar ${TARGET} >> ${LOG_APPLICATION} &
 
 sleep 1
 
-echo "TYPE "tail -f -n1000 ${LOG_RUN}" IN THE TERMINAL"
+echo "TYPE "tail -f -n1000 ${LOG_APPLICATION}" AT TERMINAL"
