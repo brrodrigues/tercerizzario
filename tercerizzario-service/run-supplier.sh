b52016f3c628f5cd756c7f73a259b758fac3a7fa
Application User <appuser@tercerizzario.localdomain>
2017-04-09 03:10:48 +0000

    x-shellscript

--- tercerizzario-service/run-supplier.sh
+++ tercerizzario-service/run-supplier.sh
@@ -2,30 +2,34 @@
 
 NUM_ARGS=$#
 
-PROJECT_NAME="supplier"
-EUREKA_SERVER=/home/appuser/app/tercerizzario/tercerizzario-service
-APPLICATION_NAME="tercerizzario-service-${PROJECT_NAME}"
+PROJECT_NAME="service-supplier"
+APPLICATION_PATH=/home/appuser/app/tercerizzario/tercerizzario-service
+APPLICATION_NAME="tercerizzario-${PROJECT_NAME}"
 LOG_APPLICATION="$APPLICATION_NAME.log"
 LOG_COMPILE_MAVEN="${APPLICATION_NAME}_mvn_compile.log"
 
 echo 'FINDING LOCATION PATH'
 
-if [ ! -d ~/app/logs ]; then
+if [ ! -f ~/app/logs ]; then
    echo 'CREATING LOG PATH...'
    mkdir -p ~/app/logs
 fi
 
+echo "LOCATING ${APPLICATION_NAME} APPLICATION PROCESS ID..."
+
+PID=$(ps -ef | grep "${APPLICATION_NAME}" | grep -v grep | awk '{print $2}')
+
+if [ "${PID}x" = "x" ]; then
+  echo "DO NOT EXIST ${APPLICATION_NAME} APPLICATION RUNNING" 
+else
+  echo "KILLING THE PROCESS ${PID} REFERENCE ${APPLICATION_NAME} APPLICATION"
+  kill -9 ${PID}
+  echo "DONE"
+fi
+
 echo "COMPILING PROJECT WITH MAVEN"
 
-EUREKA_SERVER_PATH=$(pwd)
-
-if [ -f $LOG_APPLICATION  ]; then
- rm $LOG_APPLICATION
-fi
-
-if [ -f $LOG_COMPILE_MAVEN ]; then
-  rm $LOG_COMPILE_MAVEN
-fi
+cd ${APPLICATION_PATH}/$APPLICATION_NAME
 
 mvn clean package >> $LOG_COMPILE_MAVEN
 
@@ -37,29 +41,17 @@ if [ "${COMPILED}x" != "0x"  ]; then
 	exit -1
 fi
 
-echo 'LOCATING APPLICATION PROCESS ID...'
+#TARGET=$(ls -a ${EUREKA_SERVER_PATH}/$APPLICATION_NAME/target/*.jar)
+#
+#if [ ! -f ${TARGET} ]; then
+#  echo "${APPLICATION_NAME} APPLICATION NOT FOUND"
+#  exit -1
+#fi
 
-PID=$(ps -ef | grep "${APPLICATION_NAME}" | grep -v grep | awk '{print $2}')
+echo "STARTING ${APPLICATION_NAME}..."
 
-if [ "${PID}x" = "x" ]; then
-  echo 'DO NOT EXIST APPLICATION SERVICE' 
-else
-  echo "KILLING THE PROCESS ${PID} REFERENCE APPLICATION"
-  kill -9 ${PID}
-  echo "DONE"
-fi
-
-TARGET=$(ls -a ${EUREKA_SERVER_PATH}/$APPLICATION_NAME/target/${APPLICATION_NAME}*.jar)
-
-if [ ! -f ${TARGET} ]; then
-  echo 'SERVER APPLICATION NOT FOUND'
-  exit -1
-fi
-
-echo 'STARTING ...'
-
-nohup java -Xmx256m -Xss256k -jar ${TARGET} >> ${LOG_APPLICATION} &
+mvn spring-boot:run >> ${LOG_APPLICATION} &
 
 sleep 1
 
-echo "TYPE "tail -f -n1000 ${LOG_APPLICATION}" AT TERMINAL"
+echo "TYPE tail -f -n1000 ${APPLICATION_PATH}/${APPLICATION_NAME}/${LOG_APPLICATION} AT TERMINAL"
