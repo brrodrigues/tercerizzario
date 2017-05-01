333ddf0cdf62d40817f8062f77fff4bf3fabe1e5
Application User <appuser@tercerizzario.localdomain>
2017-05-01 17:01:13 +0000

    adjusting run-gateway...

--- tercerizzario-service/run-gateway.sh
+++ tercerizzario-service/run-gateway.sh
@@ -3,8 +3,7 @@
 NUM_ARGS=$#
 
 PROJECT_NAME="gateway"
-UPPER_PROJECT_NAME=${PROJECT_NAME^^}
-EUREKA_SERVER_PATH=/home/appuser/app/tercerizzario/tercerizzario-service
+APPLICATION_PATH=/home/appuser/app/tercerizzario/tercerizzario-service
 APPLICATION_NAME="tercerizzario-${PROJECT_NAME}"
 LOG_APPLICATION="$APPLICATION_NAME.log"
 LOG_COMPILE_MAVEN="${APPLICATION_NAME}_mvn_compile.log"
@@ -16,9 +15,21 @@ if [ ! -f ~/app/logs ]; then
    mkdir -p ~/app/logs
 fi
 
+echo "LOCATING ${APPLICATION_NAME} APPLICATION PROCESS ID..."
+
+PID=$(ps -ef | grep "${APPLICATION_NAME}" | grep -v grep | awk '{print $2}')
+
+if [ "${PID}x" = "x" ]; then
+  echo "DO NOT EXIST ${APPLICATION_NAME} APPLICATION RUNNING" 
+else
+  echo "KILLING THE PROCESS ${PID} REFERENCE ${APPLICATION_NAME} APPLICATION"
+  kill -9 ${PID}
+  echo "DONE"
+fi
+
 echo "COMPILING PROJECT WITH MAVEN"
 
-cd ${EUREKA_SERVER_PATH}/$APPLICATION_NAME
+cd ${APPLICATION_PATH}/$APPLICATION_NAME
 
 mvn clean package >> $LOG_COMPILE_MAVEN
 
@@ -30,29 +41,17 @@ if [ "${COMPILED}x" != "0x"  ]; then
 	exit -1
 fi
 
-echo "LOCATING ${UPPER_PROJECT_NAME} APPLICATION PROCESS ID..."
-
-PID=$(ps -ef | grep "${APPLICATION_NAME}" | grep -v grep | awk '{print $2}')
-
-if [ "${PID}x" = "x" ]; then
-  echo "DO NOT EXIST ${UPPER_PROJECT_NAME} APPLICATION SERVER RUNNING" 
-else
-  echo "KILLING THE PROCESS ${PID} REFERENCE ${UPPER_PROJECT_NAME}"
-  kill -9 ${PID}
-  echo "DONE"
-fi
-
-#TARGET=$(ls -a ${EUREKA_SERVER_PATH}/$APPLICATION_NAME/target/)
+#TARGET=$(ls -a ${EUREKA_SERVER_PATH}/$APPLICATION_NAME/target/*.jar)
 #
 #if [ ! -f ${TARGET} ]; then
-#  echo "${UPPER_PROJECT_NAME} APPLICATION NOT FOUND"
+#  echo "${APPLICATION_NAME} APPLICATION NOT FOUND"
 #  exit -1
 #fi
 
-echo "STARTING ${UPPER_PROJECT_NAME}"
+echo "STARTING ${APPLICATION_NAME}..."
 
 mvn spring-boot:run >> ${LOG_APPLICATION} &
 
 sleep 1
 
-echo "TYPE tail -f -n1000 ${LOG_APPLICATION} AT TERMINAL"
+echo "TYPE tail -f -n1000 ${APPLICATION_PATH}/${APPLICATION_NAME}/${LOG_APPLICATION} AT TERMINAL"
